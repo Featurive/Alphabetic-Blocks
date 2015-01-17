dcaceea8c2968773fb03270a4d15c97157e441c8
featurive <bram.agjm1@gmail.com>
2015-01-17 23:56:11 +0100

    Initial commit

---
+++ src/main/java/com/featurive/alphabet/Alphabet.java
@@ -0,0 +1,14 @@
+package com.featurive.alphabet;
+
+import com.featurive.alphabet.reference.References;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = References.MODID, version = References.VERSION, name = References.MODNAME)
+public class Alphabet{
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+}
